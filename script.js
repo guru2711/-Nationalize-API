381fb5faecdfb18f22a38878f661affda0f57617
guru2711 <saiguruprakash@gmail.com>
2021-08-25 01:17:15 +0530

    nationalize api

---
+++ script.js
@@ -0,0 +1,22 @@
+const name = document.getElementById("mytext").value;
+const btn = document.querySelector(".btn");
+
+async function get(name) {
+  const data = await fetch(`https://api.nationalize.io/?name=${name}`);
+  const display = await data.json();
+  const show = await create(display.country);
+  //   console.log(show);
+}
+
+function create(show) {
+  const para = document.querySelector("#display");
+  para.innerText = `country: ${show[0]["country_id"]} => probability: ${show[0].probability}
+  country: ${show[1]["country_id"]} => probability: ${show[1].probability}`;
+  document.body.append(para);
+}
+
+btn.addEventListener("click", () => {
+  //   console.log(value);
+  window.onload;
+  get(name);
+});

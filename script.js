6c512f8fa4c6e0da38b2acd2637d9c5d5ab98748
guru2711 <saiguruprakash@gmail.com>
2021-08-25 12:44:22 +0530

    "third update"

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-const names = document.getElementById("mytext").value;
-const btn = document.querySelector(".btn");
+function myFunction() {
+  var x = document.getElementById("myText").value;
+  // document.getElementById("display").innerHTML = x;
+  // console.log(x)
+  get(x);
+}
 
-btn.addEventListener("click", () => {
-  //   console.log(value);
-
-  get(names);
-});
-
-async function get(name) {
-  const data = await fetch(`https://api.nationalize.io/?name=${name}`);
+async function get(myFunction) {
+  const data = await fetch(`https://api.nationalize.io/?name=${myFunction}`);
   const display = await data.json();
-  const show = await create(display.country);
-  //   console.log(show);
+  const show = await create(display);
+  console.log(show);
 }
 
 function create(show) {
   const para = document.querySelector("#display");
-  para.innerText = `country: ${show[0]["country_id"]} => probability: ${show[0].probability}
-  country: ${show[1]["country_id"]} => probability: ${show[1].probability}`;
+  para.innerText = `country: ${show.country[0]["country_id"]} => probability: ${show.country[0].probability}
+        country: ${show.country[1]["country_id"]} => probability: ${show.country[1].probability}`;
   document.body.append(para);
 }
